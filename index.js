ce81de7ba83ed9dc246b1c36edd5871cfce5f1d6
Dzmitry Khamitsevich <dzmitry_khamitsevich@epam.com>
2022-06-17 21:07:07 +0300

    modify slider

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const firstControl = document.getElementById("first_item");
 const secondControl = document.getElementById("second_item");
 const thirdControl = document.getElementById("third_item");
 
+const firstPicture = document.getElementById('first_slide');
+const secondPicture = document.getElementById('second_slide');
+const thirdPicture = document.getElementById('third_slide')
+
 const sliderContainer = document.querySelector(".slides_container")
 
 const CHOSEN_CLASS = 'chosen_control'
 
-firstControl.addEventListener('click', () => {
+const showFirst = () => {
     sliderContainer.style.transform = "translateX(60%)";
     secondControl.classList.remove(CHOSEN_CLASS)
     thirdControl.classList.remove(CHOSEN_CLASS)
     firstControl.classList.add(CHOSEN_CLASS)
-})
-secondControl.addEventListener('click', () => {
+}
+
+const showSecond = () => {
     sliderContainer.style.transform = "translateX(0%)"
     secondControl.classList.add(CHOSEN_CLASS)
     thirdControl.classList.remove(CHOSEN_CLASS)
     firstControl.classList.remove(CHOSEN_CLASS)
-})
-thirdControl.addEventListener('click', () => {
+}
+
+const showThird = () => {
     sliderContainer.style.transform = "translateX(-60%)"
     secondControl.classList.remove(CHOSEN_CLASS)
     thirdControl.classList.add(CHOSEN_CLASS)
     firstControl.classList.remove(CHOSEN_CLASS)
-})
+}
+
+firstPicture.addEventListener('click', showFirst)
+secondPicture.addEventListener('click', showSecond)
+thirdPicture.addEventListener('click', showThird)
+
+firstControl.addEventListener('click', showFirst)
+secondControl.addEventListener('click', showSecond)
+thirdControl.addEventListener('click', showThird)
